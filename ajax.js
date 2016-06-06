768cd85d95b7ca5d2de2df766a29f3ca3bc360c6
Daniel Breen <daniel@akirix.com>
2016-06-06 14:55:26 -0600

    WIP Ajax requests

---
+++ ajax.js
@@ -0,0 +1,42 @@
+var api = 'http://localhost:3000';
+
+$( '#test-getShifts' ).click( function(){
+    $.ajax( {
+        type: 'GET',
+        url: api + '/shifts'
+    } )
+        .success( function( result ){
+            var shifts = result.shifts;
+            shifts.forEach( function( shift ){
+                console.log( shift );
+            } );
+        } )
+        .error( function( err ){
+            console.log( err );
+        } );
+} );
+
+$( '#test-postShifts' ).click( function(){
+    var data = JSON.stringify( {
+        shift: {
+            user_id: "1a2b",
+            shift_date: "2016-05-03 18:14:55",
+            type: "pto",
+            hours: 8,
+            pay_period: 5
+        }
+    } );
+    $.ajax( {
+        type: 'POST',
+        url: api + '/shifts',
+        contentType: 'application/json',
+        data: data
+    } )
+        .success( function( result ){
+            console.log(result.shift);
+        } )
+        .error( function( err ){
+            console.log( err );
+        } );
+} )
+;
