868ef20f7813b57423ffad8b4367ccec90d0ff77
Daniel Breen <daninreallife7@gmail.com>
2016-07-18 23:25:58 -0600

    Polish

--- ajax.js
+++ ajax.js
@@ -54,27 +54,3 @@ $('#shifts-submit').click(function() {
             console.log(err);
         });
 });
-
-$('#test-postShifts').click(function() {
-    var data = JSON.stringify({
-        shift: {
-            user_id: "1a2b",
-            shift_date: "2016-05-03 18:14:55",
-            type: "pto",
-            hours: 8,
-            pay_period: 5
-        }
-    });
-    $.ajax({
-            method: 'POST',
-            url: api + '/shifts',
-            contentType: 'application/json',
-            data: data
-        })
-        .success(function(result) {
-            console.log(result.shift);
-        })
-        .error(function(err) {
-            console.log(err);
-        });
-});
